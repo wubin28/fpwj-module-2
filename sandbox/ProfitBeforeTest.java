c215123d950d94991fc6be19904a4fe64444f3ea
lobster_23EB21BF08 <lobster@cyber-dojo.org>
2016-02-10 03:07:25 +0000

    1

---
+++ sandbox/ProfitBeforeTest.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ProfitBeforeTest {
+
+    @Test
+    public void life_the_universe_and_everything() {
+        assertEquals(1, 1);
+    }
+}
