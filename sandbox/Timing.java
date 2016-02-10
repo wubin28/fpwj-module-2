d8bdd81f13bc6e290bf5f102fcbf0b0b7cee5349
lobster_23EB21BF08 <lobster@cyber-dojo.org>
2016-02-10 05:55:05 +0000

    33

--- sandbox/Timing.java
+++ sandbox/Timing.java
@@ -4,7 +4,8 @@ import java.util.function.Supplier;
 
 public class Timing {
 
-    public static Double timedDouble(String description, Supplier<Double> code) {
+    public static Double timedDouble(String description, 
+            Supplier<Double> code) {
         final Date before = new Date();
         final Double result = code.get();
         final Long duration = new Date().getTime() - before.getTime();
@@ -14,12 +15,23 @@ public class Timing {
 
     public static <A> A timed(String description,
                               Supplier<A> code) {
+        final Consumer<String> defaultPrinter = System.out::println;
+
+        // change default to do nothing 
+        // final Consumer<String> defaultPrinter = (s) -> {};  
+
+        return timed(description, defaultPrinter, code);
+    }
+
+    public static <A> A timed(String description,
+                              Consumer<String> printer,
+                              Supplier<A> code) {
         final Date before = new Date();
 
         final A result = code.get();
 
         final Long duration = new Date().getTime() - before.getTime();
-        System.out.println(description + " took " + duration + " ms");
+        printer.accept(description + " took " + duration + " ms");
         return result;
     }
 }
